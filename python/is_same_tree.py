ae73c19e754221be94995dea113b26f13e5c7c71
Marat Mukhutdinov <mmuhutdinovv@gmail.com>
2022-06-24 13:01:14 +0800

    isSameTree on python

---
+++ python/is_same_tree.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p == None and q == None:
+            return True
+        if p == None or q == None:
+            return False
+
+        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+
+tree1 = TreeNode(1, TreeNode(2), TreeNode(3))
+tree2 = TreeNode(1, TreeNode(2), TreeNode(3))
+sol = Solution()
+print(sol.isSameTree(tree1, tree2))

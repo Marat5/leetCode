19771688c66b4e4b6df2ed0299e67a68527bc6c9
Marat Mukhutdinov <mmuhutdinovv@gmail.com>
2022-09-17 23:19:58 +0300

    pascal triangle

---
+++ python/pascal_triangle.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        rows = [[1] * (i + 1) for i in range(numRows)]
+        for i in range(2, numRows):
+            for j in range(1, i):
+                rows[i][j] = rows[i-1][j] + rows[i-1][j-1]
+        return rows
+
+
+print(Solution().generate(5))

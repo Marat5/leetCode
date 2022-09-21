53a5586cc8893a779c4ac81c90bdff6c71f66222
Marat Mukhutdinov <mmuhutdinovv@gmail.com>
2022-09-22 00:02:16 +0300

    Palindrome with two pointers

--- python/is_palindrome.py
+++ python/is_palindrome.py
@@ -3,18 +3,33 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         # Stack solution
+        # s = s.lower()
+        # alpha_numeric_stack = []
+        # for c in s:
+        #     c.lower()
+        #     if c.isalnum():
+        #         alpha_numeric_stack.append(c)
+
+        # for c in s:
+        #     if c.isalnum() and c != alpha_numeric_stack.pop():
+        #         return False
+
+        # return True
+
+        # Two pointers solution
         s = s.lower()
-        alpha_numeric_stack = []
-        for c in s:
-            c.lower()
-            if c.isalnum():
-                alpha_numeric_stack.append(c)
-
-        for c in s:
-            if c.isalnum() and c != alpha_numeric_stack.pop():
+        l = 0
+        r = len(s) - 1
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while l < r and not s[r].isalnum():
+                r -= 1
+            if s[l] != s[r]:
                 return False
-
+            l += 1
+            r -= 1
         return True
 
 
-print(Solution().isPalindrome("A man, a plan, a canal: Panama"))
+print(Solution().isPalindrome(".,"))

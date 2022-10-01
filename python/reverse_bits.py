90faf4969e12f8089732ab8244283c9d46d00d8e
Marat Mukhutdinov <mmuhutdinovv@gmail.com>
2022-10-01 23:47:15 +0300

    reverse bits

---
+++ python/reverse_bits.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reverseBits(self, n: str) -> int:
+        s = format(n, "32b")
+        arr = []
+        for i in s:
+            if i == "0" or i == " ":
+                arr.append("0")
+            else:
+                arr.append("1")
+        arr = reversed(arr)
+        return int("".join(arr), 2)
+
+
+print(Solution().reverseBits(0b00000010100101000001111010011100))

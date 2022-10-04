530052a57245779c2e1df255bc7b1ac39f8c31ec
Marat Mukhutdinov <mmuhutdinovv@gmail.com>
2022-10-04 14:06:39 +0300

    Added BFS solution for path sum problem

--- python/path_sum.py
+++ python/path_sum.py
@@ -10,6 +10,7 @@ class TreeNode:
 
 
 class Solution:
+    # DFS, preorder tree traversal
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
         if not root:
             return False
@@ -19,3 +20,24 @@ class Solution:
             return targetSumForSubTrees == 0
 
         return self.hasPathSum(root.left, targetSumForSubTrees) or self.hasPathSum(root.right, targetSumForSubTrees)
+
+    # BFS, preorder tree traversal (slower, I made it to practice bfs)
+    # def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+    #     queue = [(root, targetSum)]
+
+    #     while queue:
+    #         current_node, target = queue.pop(0)
+    #         if not current_node:
+    #             continue
+
+    #         if not current_node.left and not current_node.right and target == current_node.val:
+    #             return True
+
+    #         queue.append((current_node.left, target - current_node.val))
+    #         queue.append((current_node.right, target - current_node.val))
+
+    #     return False
+
+
+print(Solution().hasPathSum(TreeNode(12, TreeNode(
+    2, TreeNode(11), TreeNode(12)), TreeNode(3, TreeNode(1), TreeNode(2))), 16))
